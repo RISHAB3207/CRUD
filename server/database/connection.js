76e66e80048641f129730385c9b5c84b2f5b1896
Rishab Chaudhary <rishab3207@gmail.com>
2022-06-06 17:22:23 +0530

    initial commit

---
+++ server/database/connection.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try{
+        // mongodb connection string
+        const con = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+        })
+
+        console.log(`MongoDB connected : ${con.connection.host}`);
+    }catch(err){
+        console.log(err);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB
+
+// const mongoose=require('mongoose');
+// const connectDB=async()=>{
+//     try {
+        
+//         const con = await mongoose.connect(process.env.MONGO_URI, {
+            
+//         })
+    
+//     console.log(`MongoDB connected :${con.connection.host}`);
+//     } catch (error) {
+//         console.log(error);
+//         process.exit(1);
+                    
+        
+//     }
+// }
+
+// module.exports=connectDB;
